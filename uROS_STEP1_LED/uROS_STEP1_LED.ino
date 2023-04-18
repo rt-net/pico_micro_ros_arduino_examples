3d56137eb648f94ba9e0434e2f41fd2443c58bfb
ShotaAk <s.aoki@rt-net.jp>
2023-04-18 11:16:56 +0900

    .clang-formatファイルをルートに追加。 STEP1に.clang-formatを追加。 STEP1にArduino-IDEの自動整形を適用

--- uROS_STEP1_LED/uROS_STEP1_LED.ino
+++ uROS_STEP1_LED/uROS_STEP1_LED.ino
@@ -17,26 +17,26 @@
 #define LED2 42
 #define LED3 41
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(LED0,OUTPUT);
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
-
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  digitalWrite(LED0,HIGH);
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED2,HIGH);
-  digitalWrite(LED3,HIGH);
+  digitalWrite(LED0, HIGH);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
   delay(500);
-  digitalWrite(LED0,LOW);
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED3,LOW);
+  digitalWrite(LED0, LOW);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
   delay(500);
 }
