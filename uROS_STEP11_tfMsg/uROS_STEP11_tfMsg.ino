0f1ea2b8f5bc28f52d526134e0df46941d4961c1
ShotaAk <s.aoki@rt-net.jp>
2023-04-19 15:50:11 +0900

    STEP11 グローバル変数に接頭辞g_を追加、変数末尾にr, lをつける

--- uROS_STEP11_tfMsg/uROS_STEP11_tfMsg.ino
+++ uROS_STEP11_tfMsg/uROS_STEP11_tfMsg.ino
@@ -26,18 +26,18 @@
 #include <tf2_msgs/msg/tf_message.h>
 // clang-format on
 
-geometry_msgs__msg__Twist msg;
-tf2_msgs__msg__TFMessage * tf_message;
-sensor_msgs__msg__JointState jstate;
-rosidl_runtime_c__String joint_name[2];
-double positions[2];
+geometry_msgs__msg__Twist g_msg;
+tf2_msgs__msg__TFMessage * g_tf_message;
+sensor_msgs__msg__JointState g_jstate;
+rosidl_runtime_c__String g_joint_name[2];
+double g_positions[2];
 
-rcl_subscription_t subscriber;
-rcl_publisher_t publisher, publisher2;
-rclc_executor_t executor;
-rcl_allocator_t allocator;
-rclc_support_t support;
-rcl_node_t node;
+rcl_subscription_t g_subscriber;
+rcl_publisher_t g_publisher, g_publisher2;
+rclc_executor_t g_executor;
+rcl_allocator_t g_allocator;
+rclc_support_t g_support;
+rcl_node_t g_node;
 
 #define LED0 1
 #define LED1 2
@@ -56,31 +56,31 @@ rcl_node_t node;
 #define MIN_SPEED (MIN_HZ * PULSE)
 #define TREAD_WIDTH (65.0)
 
-hw_timer_t * timer0 = NULL;
-hw_timer_t * timer2 = NULL;
-hw_timer_t * timer3 = NULL;
+hw_timer_t * g_timer0 = NULL;
+hw_timer_t * g_timer2 = NULL;
+hw_timer_t * g_timer3 = NULL;
 
-portMUX_TYPE timer_mux = portMUX_INITIALIZER_UNLOCKED;
+portMUX_TYPE g_timer_mux = portMUX_INITIALIZER_UNLOCKED;
 
-unsigned short r_step_hz = MIN_HZ;
-unsigned short l_step_hz = MIN_HZ;
+unsigned short g_step_hz_r = MIN_HZ;
+unsigned short g_step_hz_l = MIN_HZ;
 
-volatile unsigned int step_r, step_l;
+volatile unsigned int g_step_r, g_step_l;
 
-volatile double max_speed = MIN_SPEED;
-volatile double min_speed = MIN_SPEED;
-volatile double r_accel = 0.0;
-volatile double speed = 0.0;
+volatile double g_max_speed = MIN_SPEED;
+volatile double g_min_speed = MIN_SPEED;
+volatile double g_accel = 0.0;
+volatile double g_speed = 0.0;
 
-double max_omega;
-double min_omega;
-double r_acc_omega = 0.0;
-double omega = 0;
-double position_r, position_l;
+double g_max_omega;
+double g_min_omega;
+double g_acc_omega = 0.0;
+double g_omega = 0;
+double g_position_r, g_position_l;
 
-double odom_x, odom_y, odom_theta;
+double g_odom_x, g_odom_y, g_odom_theta;
 
-volatile bool motor_move = 0;
+volatile bool g_motor_move = 0;
 
 #define RCCHECK(fn)                \
   {                                \
@@ -125,53 +125,53 @@ const void eulerToQuat(float x, float y, float z, double * q)
 //目標値の更新周期1kHz
 void IRAM_ATTR onTimer0(void)
 {
-  portENTER_CRITICAL_ISR(&timer_mux);  //割り込み禁止
+  portENTER_CRITICAL_ISR(&g_timer_mux);  //割り込み禁止
   controlInterrupt();
-  portEXIT_CRITICAL_ISR(&timer_mux);  //割り込み許可
+  portEXIT_CRITICAL_ISR(&g_timer_mux);  //割り込み許可
 }
 
 //Rモータの周期数割り込み
 void IRAM_ATTR isrR(void)
 {
-  portENTER_CRITICAL_ISR(&timer_mux);  //割り込み禁止
-  if (motor_move) {
-    if (r_step_hz < 30) r_step_hz = 30;
-    timerAlarmWrite(timer2, 2000000 / r_step_hz, true);
+  portENTER_CRITICAL_ISR(&g_timer_mux);  //割り込み禁止
+  if (g_motor_move) {
+    if (g_step_hz_r < 30) g_step_hz_r = 30;
+    timerAlarmWrite(g_timer2, 2000000 / g_step_hz_r, true);
     digitalWrite(PWM_R, HIGH);
     for (int i = 0; i < 100; i++) {
       asm("nop \n");
     }
     digitalWrite(PWM_R, LOW);
-    step_r++;
+    g_step_r++;
   }
-  portEXIT_CRITICAL_ISR(&timer_mux);  //割り込み許可
+  portEXIT_CRITICAL_ISR(&g_timer_mux);  //割り込み許可
 }
 
 //Lモータの周期数割り込み
 void IRAM_ATTR isrL(void)
 {
-  portENTER_CRITICAL_ISR(&timer_mux);  //割り込み禁止
-  if (motor_move) {
-    if (l_step_hz < 30) l_step_hz = 30;
-    timerAlarmWrite(timer3, 2000000 / l_step_hz, true);
+  portENTER_CRITICAL_ISR(&g_timer_mux);  //割り込み禁止
+  if (g_motor_move) {
+    if (g_step_hz_l < 30) g_step_hz_l = 30;
+    timerAlarmWrite(g_timer3, 2000000 / g_step_hz_l, true);
     digitalWrite(PWM_L, HIGH);
     for (int i = 0; i < 100; i++) {
       asm("nop \n");
     };
     digitalWrite(PWM_L, LOW);
-    step_l++;
+    g_step_l++;
   }
-  portEXIT_CRITICAL_ISR(&timer_mux);  //割り込み許可
+  portEXIT_CRITICAL_ISR(&g_timer_mux);  //割り込み許可
 }
 
 //twist message cb
 void subscriptionCallback(const void * msgin)
 {
-  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+  const geometry_msgs__msg__Twist * g_msg = (const geometry_msgs__msg__Twist *)msgin;
 
   //linearは[m/s]の単位で入力されるのでPi:Coのシステムに合わせて[mm/s]にする
-  speed = msg->linear.x * 1000.0;
-  omega = msg->angular.z;
+  g_speed = g_msg->linear.x * 1000.0;
+  g_omega = g_msg->angular.z;
 }
 
 void setup()
@@ -201,73 +201,73 @@ void setup()
 
   delay(2000);
 
-  timer0 = timerBegin(0, 80, true);  //1us
-  timerAttachInterrupt(timer0, &onTimer0, true);
-  timerAlarmWrite(timer0, 1000, true);  //1kHz
-  timerAlarmEnable(timer0);
+  g_timer0 = timerBegin(0, 80, true);  //1us
+  timerAttachInterrupt(g_timer0, &onTimer0, true);
+  timerAlarmWrite(g_timer0, 1000, true);  //1kHz
+  timerAlarmEnable(g_timer0);
 
-  timer2 = timerBegin(2, 40, true);  //0.5us
-  timerAttachInterrupt(timer2, &isrR, true);
-  timerAlarmWrite(timer2, 13333, true);  //150Hz
-  timerAlarmEnable(timer2);
+  g_timer2 = timerBegin(2, 40, true);  //0.5us
+  timerAttachInterrupt(g_timer2, &isrR, true);
+  timerAlarmWrite(g_timer2, 13333, true);  //150Hz
+  timerAlarmEnable(g_timer2);
 
-  timer3 = timerBegin(3, 40, true);  //0.5us
-  timerAttachInterrupt(timer3, &isrL, true);
-  timerAlarmWrite(timer3, 13333, true);  //150Hz
-  timerAlarmEnable(timer3);
+  g_timer3 = timerBegin(3, 40, true);  //0.5us
+  timerAttachInterrupt(g_timer3, &isrL, true);
+  timerAlarmWrite(g_timer3, 13333, true);  //150Hz
+  timerAlarmEnable(g_timer3);
 
-  allocator = rcl_get_default_allocator();
+  g_allocator = rcl_get_default_allocator();
 
   //create init_options
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  RCCHECK(rclc_support_init(&g_support, 0, NULL, &g_allocator));
 
-  // create node
-  RCCHECK(rclc_node_init_default(&node, "micro_ros_pico_node", "", &support));
+  // create g_node
+  RCCHECK(rclc_node_init_default(&g_node, "micro_ros_pico_node", "", &g_support));
 
-  //publisher
+  //g_publisher
   RCCHECK(rclc_publisher_init_default(
-    &publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(tf2_msgs, msg, TFMessage), "/tf"));
+    &g_publisher, &g_node, ROSIDL_GET_MSG_TYPE_SUPPORT(tf2_msgs, msg, TFMessage), "/tf"));
 
   RCCHECK(rclc_publisher_init_default(
-    &publisher2, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, JointState),
+    &g_publisher2, &g_node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, JointState),
     "/joint_states"));
 
   //Subscriber
   RCCHECK(rclc_subscription_init_default(
-    &subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "/cmd_vel"));
+    &g_subscriber, &g_node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "/cmd_vel"));
 
-  // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  // create g_executor
+  RCCHECK(rclc_executor_init(&g_executor, &g_support.context, 1, &g_allocator));
   RCCHECK(rclc_executor_add_subscription(
-    &executor, &subscriber, &msg, &subscriptionCallback, ON_NEW_DATA));
+    &g_executor, &g_subscriber, &g_msg, &subscriptionCallback, ON_NEW_DATA));
 
-  tf_message = tf2_msgs__msg__TFMessage__create();
-  geometry_msgs__msg__TransformStamped__Sequence__init(&tf_message->transforms, 1);
+  g_tf_message = tf2_msgs__msg__TFMessage__create();
+  geometry_msgs__msg__TransformStamped__Sequence__init(&g_tf_message->transforms, 1);
 
-  tf_message->transforms.data[0].header.frame_id.data = "/odom";
-  tf_message->transforms.data[0].header.frame_id.size =
-    strlen(tf_message->transforms.data[0].header.frame_id.data);
-  tf_message->transforms.data[0].header.frame_id.capacity = 100;
+  g_tf_message->transforms.data[0].header.frame_id.data = "/odom";
+  g_tf_message->transforms.data[0].header.frame_id.size =
+    strlen(g_tf_message->transforms.data[0].header.frame_id.data);
+  g_tf_message->transforms.data[0].header.frame_id.capacity = 100;
 
-  tf_message->transforms.data[0].child_frame_id.data = "/base_footprint";
-  tf_message->transforms.data[0].child_frame_id.size =
-    strlen(tf_message->transforms.data[0].child_frame_id.data);
-  tf_message->transforms.data[0].child_frame_id.capacity = 100;
+  g_tf_message->transforms.data[0].child_frame_id.data = "/base_footprint";
+  g_tf_message->transforms.data[0].child_frame_id.size =
+    strlen(g_tf_message->transforms.data[0].child_frame_id.data);
+  g_tf_message->transforms.data[0].child_frame_id.capacity = 100;
 
-  joint_name[0].data = "left_wheel_joint";
-  joint_name[0].size = strlen(joint_name[0].data);
-  joint_name[0].capacity = joint_name[0].size + 1;
+  g_joint_name[0].data = "left_wheel_joint";
+  g_joint_name[0].size = strlen(g_joint_name[0].data);
+  g_joint_name[0].capacity = g_joint_name[0].size + 1;
 
-  joint_name[1].data = "right_wheel_joint";
-  joint_name[1].size = strlen(joint_name[1].data);
-  joint_name[1].capacity = joint_name[1].size + 1;
+  g_joint_name[1].data = "right_wheel_joint";
+  g_joint_name[1].size = strlen(g_joint_name[1].data);
+  g_joint_name[1].capacity = g_joint_name[1].size + 1;
 
-  jstate.name.data = joint_name;
-  jstate.name.size = 2;
-  jstate.name.capacity = 2;
-  jstate.position.data = positions;
-  jstate.position.size = 2;
-  jstate.position.capacity = 2;
+  g_jstate.name.data = g_joint_name;
+  g_jstate.name.size = 2;
+  g_jstate.name.capacity = 2;
+  g_jstate.position.data = g_positions;
+  g_jstate.position.size = 2;
+  g_jstate.position.capacity = 2;
 
   digitalWrite(MOTOR_EN, HIGH);
 }
@@ -277,28 +277,28 @@ void loop()
   double q[4];
   uint32_t current = micros();
 
-  jstate.header.stamp.sec = current / 1000000;
-  jstate.header.stamp.nanosec = current - jstate.header.stamp.sec * 1000000;
+  g_jstate.header.stamp.sec = current / 1000000;
+  g_jstate.header.stamp.nanosec = current - g_jstate.header.stamp.sec * 1000000;
 
-  eulerToQuat(0, 0, odom_theta, q);
-  tf_message->transforms.data[0].transform.translation.x = odom_x;
-  tf_message->transforms.data[0].transform.translation.y = odom_y;
-  tf_message->transforms.data[0].transform.translation.z = 0.0;
+  eulerToQuat(0, 0, g_odom_theta, q);
+  g_tf_message->transforms.data[0].transform.translation.x = g_odom_x;
+  g_tf_message->transforms.data[0].transform.translation.y = g_odom_y;
+  g_tf_message->transforms.data[0].transform.translation.z = 0.0;
 
-  tf_message->transforms.data[0].transform.rotation.x = (double)q[1];
-  tf_message->transforms.data[0].transform.rotation.y = (double)q[2];
-  tf_message->transforms.data[0].transform.rotation.z = (double)q[3];
-  tf_message->transforms.data[0].transform.rotation.w = (double)q[0];
-  tf_message->transforms.data[0].header.stamp.nanosec = jstate.header.stamp.nanosec;
-  tf_message->transforms.data[0].header.stamp.sec = jstate.header.stamp.sec;
+  g_tf_message->transforms.data[0].transform.rotation.x = (double)q[1];
+  g_tf_message->transforms.data[0].transform.rotation.y = (double)q[2];
+  g_tf_message->transforms.data[0].transform.rotation.z = (double)q[3];
+  g_tf_message->transforms.data[0].transform.rotation.w = (double)q[0];
+  g_tf_message->transforms.data[0].header.stamp.nanosec = g_jstate.header.stamp.nanosec;
+  g_tf_message->transforms.data[0].header.stamp.sec = g_jstate.header.stamp.sec;
 
-  jstate.position.data[0] = position_l;
-  jstate.position.data[1] = position_r;
+  g_jstate.position.data[0] = g_position_l;
+  g_jstate.position.data[1] = g_position_r;
 
-  RCSOFTCHECK(rcl_publish(&publisher, tf_message, NULL));
-  RCSOFTCHECK(rcl_publish(&publisher2, &jstate, NULL));
+  RCSOFTCHECK(rcl_publish(&g_publisher, g_tf_message, NULL));
+  RCSOFTCHECK(rcl_publish(&g_publisher2, &g_jstate, NULL));
 
-  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));  //for subscription
+  RCCHECK(rclc_executor_spin_some(&g_executor, RCL_MS_TO_NS(100)));  //for subscription
 
   delay(10);
 }
